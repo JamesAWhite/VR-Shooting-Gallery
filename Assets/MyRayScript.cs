ae0049d2d8de8de28f5131c134e8024b341296e9
BigSweatyHotWing <55860126+BigSweatyHotWing@users.noreply.github.com>
2021-11-05 22:56:56 -0400

    A few days' work
    Started work on shootable moving targets and score.

---
+++ Assets/MyRayScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRayScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int score = 0;
+    public SpawnObject objectSpawner;
+    RaycastHit hit;
+    public void Cast()
+    {
+        int layerMask = 1 << 3;
+
+        if (Physics.Raycast(this.gameObject.transform.position, this.gameObject.transform.forward, out hit, Mathf.Infinity, layerMask))
+        {
+            if (hit.transform.gameObject.name.Contains("shittydude"))
+            {
+                score++;
+                Debug.Log(score);
+            }
+            Debug.Log(hit.transform.gameObject.name);
+        }
+        else
+        {
+            Debug.Log("No Hit");
+        }
+    }
+}
