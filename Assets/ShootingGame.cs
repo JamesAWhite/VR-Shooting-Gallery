aca09b66a9f6752e8c03a41d4282b43e10e7741d
BigSweatyHotWing <55860126+BigSweatyHotWing@users.noreply.github.com>
2021-11-15 17:38:19 -0500

    Update ShootingGame.cs

--- Assets/ShootingGame.cs
+++ Assets/ShootingGame.cs
@@ -7,7 +7,7 @@ public class ShootingGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        guysLeft = 10
+        guysLeft = 10;
     }
     // Update is called once per frame
     void Update()
